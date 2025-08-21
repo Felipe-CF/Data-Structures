0824417787dee838e9c53f2b64741198b66522bf
Felipe-CF <prog.study.fcf@gmail.com>
2025-08-21 13:40:53 -0300

    selection sort

--- Ordenacao/Program.cs
+++ Ordenacao/Program.cs
@@ -2,12 +2,31 @@
 
 class Program
 {
-    static int[] SelectionSort()
+    static int[] SelectionSort(int[] array)
     {
-        
+        int min;
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            min = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[min] > array[j])
+                    min = j;
+            }
+            int aux = array[i];
+            array[i] = array[min];
+            array[min] = aux;
+        }
+
+        return array;
     }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] array = [7, 3, 8, 9, 2, 1];
+
+        array = SelectionSort(array);
+
+        foreach (int i in array)
+            Console.Write($"{i} ");
     }
 }
